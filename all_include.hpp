1114552c61c9f177b172a470471e8bdc6296b11c
tutu-san <tumuri4071.dev@gmail.com>
2024-05-02 01:36:36 +0900

    add all_include.hpp

---
+++ all_include.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef ALL_INClUDE
+#define ALL_INClUDE
+
+#include "amt102_encoder/amt102_encoder.hpp"
+#include "motor/motor_rotation.hpp"
+#include "pi_class/pi_class.hpp"
+#include "robomas_encoder/robomas_encoder.hpp"
+#include "robomas_rotation/robomas_rotation.hpp"
+#include "switch_filter/switch_filter.hpp"
+#include "tools/tools.hpp"
+#endif
