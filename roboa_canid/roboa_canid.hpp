47c213ee24802beb2e4bc3652a53706cb62de2a4
tutu-san <tumuri4071.dev@gmail.com>
2024-06-01 15:15:56 +0900

    add canid_generater()

--- roboa_canid/roboa_canid.hpp
+++ roboa_canid/roboa_canid.hpp
@@ -7,9 +7,14 @@
 
 //board_select
 #define CCC_BOARD
+enum class board_select_enum : uint64_t{
+    power_board,
+    ccc_board
+};
 
 //(common) board_number
-constexpr static std::array<uint64_t, 4> board_number_arr = {
+constexpr static std::array<uint64_t, 5> board_number_arr = {
+    0x00000000,
     0x00010000,
     0x00020000,
     0x00030000,
@@ -19,7 +24,6 @@ constexpr static uint64_t board_number = board_number_arr[0];
 
 //power_board
 #ifdef POWER_BOARD
-constexpr static uint64_t board_id = 0x00000000;
 
 enum class contents_type_t : int{
     none,
@@ -50,7 +54,6 @@ enum class ems_state : uint8_t{
 
 //ccc_board
 #ifdef CCC_BOARD
-constexpr static uint64_t board_id = 0x00000000;
 
 //motor_select
 constexpr static std::array<uint64_t, 5>motor_number_arr = {
@@ -63,57 +66,33 @@ constexpr static std::array<uint64_t, 5>motor_number_arr = {
 constexpr static uint64_t motor_number = motor_number_arr[0];
 
 //contents select
-enum class contents_name_t : int{
-    none,
-    motor_type,
-    control_mode,
-    gear_ratio,
-    can_timeout,
-    pwm_now_data,
-    pwm_terget,
-    spd_now_data,
-    spd_target,
-    spd_pwm_limit,
-    spd_gain_p,
-    spd_gain_i,
-    spd_gain_d,
-    spd_integral_reset,
-    pos_now_data,
-    pos_target,
-    pos_spd_limit,
-    pos_gain_p,
-    pos_gain_i,
-    pos_gain_d,
-    pos_integral_reset,
-    monitor_period,
-    monitor_reg
+enum class contents_name_enum : uint64_t{
+    none = 0x00,
+    motor_type = 0x01,
+    control_mode = 0x02,
+    gear_ratio = 0x03,
+    can_timeout = 0x04,
+    pwm_now_data = 0x10,
+    pwm_terget = 0x11,
+    spd_now_data = 0x20,
+    spd_target = 0x21,
+    spd_pwm_limit = 0x22,
+    spd_gain_p = 0x23,
+    spd_gain_i = 0x24,
+    spd_gain_d = 0x25,
+    spd_integral_reset = 0x26,
+    pos_now_data = 0x30,
+    pos_target = 0x31,
+    pos_spd_limit = 0x32,
+    pos_gain_p = 0x33,
+    pos_gain_i = 0x34,
+    pos_gain_d = 0x35,
+    pos_integral_reset = 0x36,
+    monitor_period = 0xF0,
+    monitor_reg = 0xF1
 };
 
-constexpr static std::array<uint64_t, 23>contents_name_arr = {
-    0x00, //none
-    0x01, //motor_type (motor_type)
-    0x02, //control_mode (control_mode)
-    0x03, //gear_ratio
-    0x04, //can_timeout (common/注:どのモーターidで来るかわからないので、共通で設定しておくこと)
-    0x10, //pwm_now_data (現在出力中のpwm数値を送る(0.0~1.0/ float))
-    0x11, //pwm_terget (pwm指令値(from pc 0.0~1.0/ float))
-    0x20, //spd_now_data (現在のrad/s値を送る(float))
-    0x21, //spd_target (目標rad/s値を受信(float))
-    0x22, //spd_pwm_limit (トルク制限(おそらく電流？))
-    0x23, //spd_gain_p (速度pidのPゲイン変更)
-    0x24, //spd_gain_i (速度pidのIゲイン変更)
-    0x25, //spd_gain_d (速度pidのDゲイン変更)
-    0x26, //spd_integral_reset (速度PID制御器, 累積値をリセット / for 停止時など)
-    0x30, //pos_now_data (現在の角度値を送信(rad) / PCからの受信の場合は、現在の角度基準設定)
-    0x31, //pos_target (位置PIDの目標角度を受信(rad))
-    0x32, //pos_spd_limit(速度制限(エンコーダー暴走防止用？))
-    0x33, //pos_gain_p (位置pidのPゲイン変更)
-    0x34, //pos_gain_i (位置pidのIゲイン変更)
-    0x35, //pos_gain_d (位置pidのDゲイン変更)
-    0x36, //pos_integral_reset(位置PID制御器, 累積値をリセット)
-    0xF0, //monitor_period (board -> pc feedback period)
-    0xF1  //monitor_reg (monitor_data select)
-};
+
 
 //can_data と比較するやつ
 enum class motor_type : uint8_t {
@@ -127,5 +106,9 @@ enum class control_mode : uint8_t {
     POS
 };
 #endif
+
+uint64_t canid_generater(board_select_enum board, contents_name_enum contents_name, int board_number = 1){
+    return static_cast<uint64_t>(board) + static_cast<uint64_t>(contents_name) + board_number_arr[board_number];
+}
 
 #endif
