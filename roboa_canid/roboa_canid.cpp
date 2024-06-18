ec3ed10ccf89ee11b19ab1c8bfe228c06c8ec765
tutu-san <tumuri4071.dev@gmail.com>
2024-06-18 18:23:29 +0900

    fix typo

--- roboa_canid/roboa_canid.cpp
+++ roboa_canid/roboa_canid.cpp
@@ -5,7 +5,7 @@ uint64_t canid_generater_power(read_or_write_enum read_or_write, power_contents_
     return static_cast<uint64_t>(read_or_write) + static_cast<uint64_t>(board_select_enum::power_board) + static_cast<uint64_t>(contents_name) + board_number_arr[board_number];
 }
 
-uint64_t canid_generater_ccc(read_or_write_enum read_or_write, ccc_contents_name_enum contents_name, int motor_number, int board_number,){
+uint64_t canid_generater_ccc(read_or_write_enum read_or_write, ccc_contents_name_enum contents_name, int motor_number, int board_number){
     return static_cast<uint64_t>(read_or_write) + static_cast<uint64_t>(board_select_enum::ccc_board) + static_cast<uint64_t>(contents_name) + board_number_arr[board_number] + motor_number_arr[motor_number];
 }
 
