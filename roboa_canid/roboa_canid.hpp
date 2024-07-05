9f08479b14ea7c5d8e79c883062fe629fae6fcd0
tutu-san <tumuri4071.dev@gmail.com>
2024-07-05 16:51:22 +0900

    fix can id

--- roboa_canid/roboa_canid.hpp
+++ roboa_canid/roboa_canid.hpp
@@ -20,12 +20,11 @@ enum class board_select_enum : uint64_t{
 };
 
 //(common) board_number
-constexpr static std::array<uint64_t, 5> board_number_arr = {
+constexpr static std::array<uint64_t, 4> board_number_arr = {
     0x00000000,
     0x00010000,
     0x00020000,
     0x00030000,
-    0x00040000
 };
 constexpr static uint64_t board_number = board_number_arr[0];
 
@@ -117,12 +116,12 @@ enum class port_mode : uint8_t {
 
 //canid_gen 
 //power
-uint64_t canid_generater_power(read_or_write_enum read_or_write, power_contents_name_enum contents_name, int board_number = 1);
+uint64_t canid_generater_power(read_or_write_enum read_or_write, power_contents_name_enum contents_name, int board_number = 0);
 
 //ccc
-uint64_t canid_generater_ccc(read_or_write_enum read_or_write, ccc_contents_name_enum contents_name, int motor_number = 0, int board_number = 1);
+uint64_t canid_generater_ccc(read_or_write_enum read_or_write, ccc_contents_name_enum contents_name, int motor_number = 0, int board_number = 0);
 
 //gpio
-uint64_t canid_generater_gpio(read_or_write_enum read_or_write, gpio_board_contents_name_enum contents_name, int board_number = 1);
+uint64_t canid_generater_gpio(read_or_write_enum read_or_write, gpio_board_contents_name_enum contents_name, int board_number = 0);
 
 #endif
