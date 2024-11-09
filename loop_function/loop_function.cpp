d4e9f75271e771024fe02d08f8d6e6b524ffca9c
tutu-san <tumuri4071.dev@gmail.com>
2024-11-09 22:48:05 +0900

    change class  name

--- loop_function/loop_function.cpp
+++ loop_function/loop_function.cpp
@@ -1,11 +1,11 @@
 #include "loop_function.hpp"
 
-void interrupt_handler_base::change_interval(int new_interval){
+void interrupt_function_base::change_interval(int new_interval){
     execution_interval = (new_interval > 0 ? new_interval : 0);
 }
 
 
-void interrupt_handler_no_argment::execute(int timer_interval_time){
+void interrupt_function_no_argment::execute(int timer_interval_time){
     if(execution_interval == 0) return; //intervalが0に設定されている場合実行されなくなる
     elapsed_time_after_execution += (timer_interval_time > 0 ? timer_interval_time : 0); //<-タイマー1実行あたりの経過時間
     if(elapsed_time_after_execution >= execution_interval){
--- loop_function/loop_function.hpp
+++ loop_function/loop_function.hpp
@@ -5,27 +5,27 @@
 #include <functional>
 #include "../tools/tools.hpp"
 
-class interrupt_handler_base{
+class interrupt_function_base{
 protected:
     int execution_interval; //ms (関数の実行間隔設定)
     int elapsed_time_after_execution = 0; //ms (前回の実行からの経過時間)
 public:
-    interrupt_handler_base(int _execution_interval){
+    interrupt_function_base(int _execution_interval){
         execution_interval = _execution_interval > 0 ? _execution_interval : 0;
     }
     void change_interval(int);
 };
 
-class interrupt_handler_no_argment : public interrupt_handler_base{
+class interrupt_function_no_argment : public interrupt_function_base{
 private:
     void (*function)();
 public:
-    interrupt_handler_no_argment(void(*_function)(), int _execution_interval)
+    interrupt_function_no_argment(void(*_function)(), int _execution_interval)
     :function(_function), interrupt_handler_base(_execution_interval){}
     void execute(int);
 };
 
-class timer_function : interrupt_handler_base{
+class timer_function : interrupt_function_base{
 private:
     void (*function)(bool);
 public:
