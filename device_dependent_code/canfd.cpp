04bb0c47a58fdf8b94a2d65af6487691daeef7ed
tutu-san <tumuri4071.dev@gmail.com>
2024-11-09 23:37:31 +0900

    using std::span at fdcan

--- device_dependent_code/canfd.cpp
+++ device_dependent_code/canfd.cpp
@@ -1,25 +1,28 @@
 #include "canfd.hpp"
 
 #if ENABLE_CANFD
-void can_send(void* _canfd_handle, uint32_t can_id, uint8_t(&send_data)[8], bool extended_id){
+void can_send(void* _canfd_handle, uint32_t can_id, std::span<uint8_t> can_data, bool extended_id){
     FDCAN_HandleTypeDef* canfd_handle = (FDCAN_HandleTypeDef*)_canfd_handle;
     FDCAN_TxHeaderTypeDef tx_header;
     
     //header settings
     tx_header.Identifier = can_id;
     tx_header.TxFrameType = FDCAN_DATA_FRAME;
-    tx_header.DataLength = FDCAN_DLC_BYTES_8;
     tx_header.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
     tx_header.BitRateSwitch = FDCAN_BRS_OFF;
     tx_header.FDFormat = FDCAN_CLASSIC_CAN;
     tx_header.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
     tx_header.MessageMarker = 0;
+
+    constexpr static uint32_t fdcan_dlcsize_format = 0x00010000;
+    tx_header.DataLength = can_data.size() * fdcan_dlcsize_format;
+
     if(extended_id){
         tx_header.IdType = FDCAN_EXTENDED_ID;
     }else{
         tx_header.IdType = FDCAN_STANDARD_ID;
     }
 
-	HAL_FDCAN_AddMessageToTxFifoQ(canfd_handle, &tx_header, send_data);
+	HAL_FDCAN_AddMessageToTxFifoQ(canfd_handle, &tx_header, can_data);
 }
 #endif
--- device_dependent_code/canfd.hpp
+++ device_dependent_code/canfd.hpp
@@ -3,11 +3,14 @@
 #define CANFD_HPP
 
 #include "main.h" //cube mx 生成コードを使用しているならこれで行けるはず
+#include <cstdint>
+#include <string>
+#include <span>
 
-#define ENABLE_CANFD 0
+#define ENABLE_CANFD 1
 
 #if ENABLE_CANFD
-void can_send(void* _canfd_handle, uint32_t can_id, uint8_t(&send_data)[8], bool extended_id = false);
+void can_send(void* _canfd_handle, uint32_t can_id, std::span<uint8_t> can_data, bool extended_id = false);
 
 //受信割り込みに関するコードをここに書くかどうか
 #endif
