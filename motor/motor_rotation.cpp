01b339874ee4df10a4455cb72d4e468ca9e3d68e
tutu-san <tumuri4071.dev@gmail.com>
2024-11-09 23:06:44 +0900

    fix redundant code at motor_rotaion

--- motor/motor_rotation.cpp
+++ motor/motor_rotation.cpp
@@ -10,10 +10,10 @@ void ucs3_rotation::rotate(float motor_pwm){
 	//motor_pwmのクランプ
 	clamp(motor_pwm, -1.0f, 1.0f);
     //正転と逆転を切り替える
-	if(reverse_swtich){
-		HAL_GPIO_WritePin(gpio_port, gpio_pin, (motor_pwm > 0.0f ? GPIO_PIN_RESET : GPIO_PIN_SET));
-	}else if(reverse_swtich){
-		HAL_GPIO_WritePin(gpio_port, gpio_pin, (motor_pwm > 0.0f ? GPIO_PIN_SET : GPIO_PIN_RESET));
+	if(reverse_swtich^(motor_pwm>0.0f)){
+		HAL_GPIO_WritePin(gpio_port, gpio_pin, GPIO_PIN_RESET);
+	}else{
+		HAL_GPIO_WritePin(gpio_port, gpio_pin, GPIO_PIN_SET);
 	}
 	//PWM出力を更新する
 	__HAL_TIM_SET_COMPARE(tim_handle, tim_channel, fabs(motor_pwm) * (tim_handle->Init.Period));
@@ -22,11 +22,8 @@ void ucs3_rotation::rotate(float motor_pwm){
 void dcmd_rotation::rotate(float motor_pwm){
 	clamp(motor_pwm, -1.0f, 1.0f);
 	bool rotate_dir = motor_pwm > 0.0f; //正転か逆転かを判断(入力値より)
-	if(reverse_swtich){ //Reverse_swtich によって、回転方向を逆転
-        rotate_dir = !rotate_dir;
-    }
 	//回転させる
-	if(rotate_dir){
+	if(rotate_dir ^ reverse_swtich){
 		__HAL_TIM_SET_COMPARE(tim_handle, tim_channel, fabs(motor_pwm) * (tim_handle->Init.Period));
 		__HAL_TIM_SET_COMPARE(tim_handle_inverse, tim_channel_inverse, 0);
 	}else{
