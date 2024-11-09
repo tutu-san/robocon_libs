09ded8586c83db3cbe24b65b05a5564cac4810c3
tutu-san <tumuri4071.dev@gmail.com>
2024-11-09 22:48:35 +0900

    fix bool at if

--- motor/motor_rotation.cpp
+++ motor/motor_rotation.cpp
@@ -10,9 +10,9 @@ void ucs3_rotation::rotate(float motor_pwm){
 	//motor_pwmのクランプ
 	clamp(motor_pwm, -1.0f, 1.0f);
     //正転と逆転を切り替える
-	if(reverse_swtich == true){
+	if(reverse_swtich){
 		HAL_GPIO_WritePin(gpio_port, gpio_pin, (motor_pwm > 0.0f ? GPIO_PIN_RESET : GPIO_PIN_SET));
-	}else if(reverse_swtich == false){
+	}else if(reverse_swtich){
 		HAL_GPIO_WritePin(gpio_port, gpio_pin, (motor_pwm > 0.0f ? GPIO_PIN_SET : GPIO_PIN_RESET));
 	}
 	//PWM出力を更新する
@@ -22,7 +22,7 @@ void ucs3_rotation::rotate(float motor_pwm){
 void dcmd_rotation::rotate(float motor_pwm){
 	clamp(motor_pwm, -1.0f, 1.0f);
 	bool rotate_dir = motor_pwm > 0.0f; //正転か逆転かを判断(入力値より)
-	if(reverse_swtich == true){ //Reverse_swtich によって、回転方向を逆転
+	if(reverse_swtich){ //Reverse_swtich によって、回転方向を逆転
         rotate_dir = !rotate_dir;
     }
 	//回転させる
