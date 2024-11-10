01330451486cdca71371ecdc731bd7be19eb9bcf
tutu-san <tumuri4071.dev@gmail.com>
2024-11-10 12:55:06 +0900

    pi_class add constructor

--- pi_class/pi_class.hpp
+++ pi_class/pi_class.hpp
@@ -8,6 +8,11 @@
  * @details 後方互換性を持たせるために、ここに pid_controllerにつながるヘッダファイルを残す
  */
 
-#include "pid_controller.hpp"
+#include "../pid_controller/pid_controller.hpp"
+
+class pi_class : public pid_controller{
+public:
+    using pid_controller::pid_controller;
+}
 
 #endif
