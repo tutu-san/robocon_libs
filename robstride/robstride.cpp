a2aaa76cc8b047e17e837d9a76ddcd8131c2b895
tutu-san <tumuri4071.dev@gmail.com>
2025-03-12 14:31:55 +0900

    add robstride (ブランチが多くて将来的につらそう)

---
+++ robstride/robstride.cpp
@@ -0,0 +1,74 @@
+#include "robstride.hpp"
+/*rotate*/
+void robstride::current_rotate(float power_rate = 0.0f, float current_order = 0.0f, bool direct_control = false){
+    union{
+        float current_f;
+        uint8_t current_u8[4];
+    } target_current;
+    
+    clamp(power_rate, -1.0f, 1.0f);
+    clamp(current_order, -90.0f, 90.0f);
+    target_current.current_f = direct_control ? current_order : (power_rate * current_limit);
+
+    uint32_t current_control_id = (0x12000000 + motor_id + (master_id<<8));
+    uint8_t send_data[8] = {0x06, 0x70, 0x00, 0x00, target_current.current_u8[0], target_current.current_u8[1], target_current.current_u8[2], target_current.current_u8[3]};
+    for(int i = 0; i<8; i++){
+    	_data[i] = send_data[i];
+    }
+    can_transmitter->can_input_transmit_buffer(current_control_id, send_data);
+    return;
+}
+
+void robstride::set_current_mode(){
+    uint32_t mode_control_id = (0x1200FD7F);
+    uint8_t send_data[5] = {0x05, 0x70, 0x00, 0x00, 0x03};
+    
+    can_transmitter->can_input_transmit_buffer(mode_control_id, send_data);
+    return;
+}
+
+void robstride::enable_motor(){
+    uint32_t enable_id = 0x300FD7F;
+    uint8_t send_data[8]{};
+    
+    can_transmitter->can_input_transmit_buffer(enable_id, send_data);
+    return;
+}
+/*encoder*/
+void robstride::input_encoder_data(uint8_t encoder_data[]){
+    angle_data = ((encoder_data[0]<<8) | encoder_data[1]) - 32768;
+    speed_data = ((encoder_data[2]<<8) | encoder_data[3]) - 32768;
+    touque_data = ((encoder_data[4]<<8) | encoder_data[5]) - 32768;
+}
+
+float robstride::show_speed(){ //rad/s
+    constexpr static float bit_16_divide = 1.0f/32768.0f;
+    return speed_data * 15.0f * bit_16_divide;
+}
+
+float robstride::show_angle(){
+    ruisekiwa = update_angle(angle_data, speed_data);
+	result_pos += (ruisekiwa - last_data);
+	last_data = ruisekiwa;
+	return result_pos;
+}
+
+void robstride::reset_angle(){
+	ruisekiwa = update_angle(angle_data, speed_data);
+	last_data = ruisekiwa;
+	return;
+}
+
+float robstride::update_angle(int16_t angle,int16_t speed){
+    bool angle_MSB = (angle > 0);
+    bool old_angle_MSB = (old_angle > 0);
+
+    if(old_angle_MSB == 1 && angle_MSB == 0 && speed > 0){
+        turn_count++;
+    }else if(old_angle_MSB == 0 && angle_MSB == 1 && speed < 0){
+        turn_count--;
+    }
+
+    old_angle = angle;
+    return angle_to_rad*(angle + turn_count*resolution);
+}
---
+++ robstride/robstride.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef ROBSTRIDE_HPP_
+#define ROBSTRIDE_HPP_
+
+#include <cmath>
+#include <cstdint>
+#include "tools.hpp"
+#include "can.hpp"
+
+class robstride{
+    //can
+    can_transmit* can_transmitter;
+    const uint8_t master_id = 0x00; //0xFD
+    const uint8_t motor_id = 0x00; //0x7F
+    //raw_data, tmp_data
+    int16_t angle_data;
+    int16_t speed_data;
+    int16_t touque_data;
+    
+    //angle_data
+    constexpr static size_t resolution_bit = 16;
+    constexpr static int resolution = 1<<resolution_bit;
+    constexpr static float angle_to_rad = 4*M_PI/(float)resolution;
+    int32_t turn_count = 0;
+    int16_t old_angle = 0;
+    float ruisekiwa = 0.0f;
+    float last_data = 0.0f;
+    float result_pos = 0.0f;
+    float update_angle(int16_t angle,int16_t speed);
+
+    //limit
+    float current_limit = 1.0f;
+public:
+    //コンストラクタ
+    robstride(can_transmit* _can_transmitter, uint8_t _master_id, uint8_t _motor_id) 
+    : can_transmitter(_can_transmitter), master_id(_master_id), motor_id(_motor_id){}
+    //can
+    void input_encoder_data(uint8_t encoder_data[]);
+    //rotation
+	void current_rotate(float, float, bool);
+	//speed
+	float show_speed();
+    //angle
+    float show_angle();
+    void reset_angle();
+    //mode_change
+    void set_current_mode();
+    void enable_motor();
+
+    uint8_t _data[8]{};
+};
+#endif
