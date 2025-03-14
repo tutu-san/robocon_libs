91bdc1dd45cab3924b2558a35f659794afd0f9fa
tutu-san <tumuri4071.dev@gmail.com>
2025-03-14 18:14:33 +0900

    add vesc

---
+++ vesc/vesc.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef VESC_HPP_
+#define VESC_HPP_
+//robomas_rotaionとほぼ同じなので、継承を使うべきだとは思ったりした
+#include "device_dependent_code/can.hpp"
+#include "device_dependent_code/canfd.hpp"
+
+#include <cstdint>
+#include <algorithm>
+#include "../tools/tools.hpp"
+#include "../robomas_rotation/robomas_rotation.hpp"
+
+class vesc_rotation{
+private:
+    can_transmit* can_transmitter;
+    uint32_t motor_id;
+public:
+    vesc_rotation(can_transmit* _can_transmitter, uint32_t _motor_id)
+    : can_transmitter(_can_transmitter), motor_id(_motor_id){}
+    void rotate(float power_ratio);
+}
