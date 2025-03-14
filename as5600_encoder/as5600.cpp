69345875306a6bd24f9ed5de84a1837d44f88605
tutu-san <tumuri4071.dev@gmail.com>
2025-03-14 16:58:01 +0900

    as5600: I2C update

--- as5600_encoder/as5600.cpp
+++ as5600_encoder/as5600.cpp
@@ -2,8 +2,7 @@
 void as5600_tool::request_encoder_data(){
     HAL_GPIO_WritePin(slave_selecter.port, slave_selecter.pin, GPIO_PIN_SET);
     rev_mode = true;
-    HAL_I2C_Master_Transmit(i2c_handle, as5600_i2c_id, (uint8_t*)as5600_angle_reg, 1, 50);
-    HAL_I2C_Master_Receive_IT(i2c_handle, as5600_i2c_id, rev_buffer_i2c, 2);
+    HAL_I2C_Mem_Read_IT(i2c_handle, as5600_i2c_id, static_cast<uint16_t>(as5600_angle_reg), I2C_MEMADD_SIZE_8BIT, rev_buffer_i2c, 2);
 }
 
 void as5600_tool::receive_encoder_data(){
